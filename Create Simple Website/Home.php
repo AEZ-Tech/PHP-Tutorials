26925b1b00503e9c346f4afbde07f52b837a28e7
AEZ-Tech <media.aez.tech@gmail.com>
2025-06-12 20:42:04 +0300

    Add files via upload

---
+++ Create Simple Website/Home.php
@@ -0,0 +1,9 @@
+
+<p class="content" >
+    
+    <u> AEZ-Tech <br></u>
+ Information & Tutorials  about the php scripting language . </p>
+ 
+ <p>The PHP version used in this tutorial is
+ <u class="php_version" ><?php print(phpversion()); ?></u></p>
+
